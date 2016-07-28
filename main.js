dad5da0bcd4069ee09842a8c89998e7e58c90a2d
Kahlil Ashanti <kahlilashanti2@Kahlils-MacBook-Air.local>
2016-07-28 14:10:12 -0700

    added navbar becomes transparent when scrolling up, the request demo form now works - will have to check when site is live

--- main.js
+++ main.js
@@ -53,6 +53,40 @@ $(document).ready(function (){
     });
     // contact form
 
+    // top navbar transparent
+    window.onscroll = fadeNav;
+    
+      function fadeNav(){
+        var offset = getScrollXY();
+        //if y offset is greater than 0, set opacity to desired value, otherwise set to 1
+        offset[1] > 0 ? setNavOpacity(0.5) : setNavOpacity(1.0);
+      }
+
+      function setNavOpacity(newOpacity){
+        var navBar = document.getElementById("top_nav");
+        navBar.style.opacity = newOpacity;
+      }
+
+      function getScrollXY(){
+        var scrOfX = 0, scrOfY =0;
+        if( typeof(window.pageYOffset) =='number'){
+          //Netscap compliant
+          scrOfY = window.pageYOffset;
+          scrOfX = window.pageXOffset;
+        }else if(document.body && (document.body.scrollLeft || document.body.scrollTop)){
+          //DOM compliant
+          scrOfY = document.body.scrollTop;
+          scrOfX = document.body.scrollLeft;
+        }else if(document.documentElement && (document.documentElement.scrollLeft ||
+          document.documentElement.scrollTop )){
+            //IE6 standards compliant
+            scrOfY = document.documentElement.scrollTop;
+            scrOfX = document.documentElement.scrollLeft;
+          }
+          return [ scrOfX, scrOfY];
+      }
+    // top navbar transparent
+
 
 
 });
